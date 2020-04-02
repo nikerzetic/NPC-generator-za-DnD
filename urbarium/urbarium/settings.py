b7ddbf298d8e17ea576e83b117dbbf3615d08314
Nik Erzetic <nikerzetic@gmail.com>
2020-04-02 18:50:20 +0200

    Django setup and project layout

--- urbarium/urbarium/settings.py
+++ urbarium/urbarium/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'generators.apps.GeneratorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
