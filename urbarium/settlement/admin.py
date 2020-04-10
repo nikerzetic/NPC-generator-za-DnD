269d1c2466db2eb22ac143b3fe3d1f3976e6e213
borutzupan <borutz.zupan@gmail.com>
2020-04-10 14:30:16 +0200

    Changed charasteristic names.

--- urbarium/settlement/admin.py
+++ urbarium/settlement/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Settlement, SettlementCharacteristic1, SettlementCharacteristic2, SettlementCharacteristic3, SettlementType
+from .models import Settlement, Location, KnownFor, Economy, SettlementType
 
 
 admin.site.register(Settlement)
-admin.site.register(SettlementCharacteristic1)
-admin.site.register(SettlementCharacteristic2)
-admin.site.register(SettlementCharacteristic3)
+admin.site.register(Location)
+admin.site.register(KnownFor)
+admin.site.register(Economy)
 admin.site.register(SettlementType)
--- urbarium/settlement/models.py
+++ urbarium/settlement/models.py
@@ -8,25 +8,25 @@ class SettlementType(models.Model):
         return self.name
 
 
-class SettlementCharacteristic1(models.Model):
+class Location(models.Model):
+    locationName = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.locationName
+
+
+class KnownFor(models.Model):
     characteristic = models.CharField(max_length=20)
 
     def __str__(self):
         return self.characteristic
 
 
-class SettlementCharacteristic2(models.Model):
-    characteristic = models.CharField(max_length=20)
+class Economy(models.Model):
+    economyTpye = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.characteristic
-
-
-class SettlementCharacteristic3(models.Model):
-    characteristic = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.characteristic
+        return self.economyType
 
 
 class Settlement(models.Model):
@@ -34,9 +34,9 @@ class Settlement(models.Model):
     name = models.CharField(max_length=50)
     population = models.IntegerField()
     category = models.ForeignKey(SettlementType, blank=True, null=True, on_delete=models.SET_NULL)
-    characteristic1 = models.ForeignKey(SettlementCharacteristic1, blank=True, null=True, on_delete=models.SET_NULL)
-    characteristic2 = models.ForeignKey(SettlementCharacteristic2, blank=True, null=True, on_delete=models.SET_NULL)
-    characteristic3 = models.ForeignKey(SettlementCharacteristic3, blank=True, null=True, on_delete=models.SET_NULL)
+    location = models.ForeignKey(Location, blank=True, null=True, on_delete=models.SET_NULL)
+    know_for = models.ForeignKey(KnownFor, blank=True, null=True, on_delete=models.SET_NULL)
+    economy = models.ForeignKey(Economy, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
