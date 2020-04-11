b5fce0bc1fbc9a747565c045c5d22d457cc95546
borutzupan <borutz.zupan@gmail.com>
2020-04-11 19:16:49 +0200

    Fixed errors.

--- urbarium/settlement/admin.py
+++ urbarium/settlement/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Settlement, Location, KnownFor, Economy, SettlementType
+from .models import Settlement, Location, KnownFor, Economy
 
 
 admin.site.register(Settlement)
 admin.site.register(Location)
 admin.site.register(KnownFor)
 admin.site.register(Economy)
-admin.site.register(SettlementType)
+
--- urbarium/settlement/models.py
+++ urbarium/settlement/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from settlementType.models import Category
 
 
-class SettlementType(models.Model):
-    name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
+# class SettlementType(models.Model):
+#     name = models.CharField(max_length=20)
+#
+#     def __str__(self):
+#         return self.name
 
 
 class Location(models.Model):
@@ -33,7 +34,7 @@ class Settlement(models.Model):
     # Django avtomatsko doloci id, ki je primary key
     name = models.CharField(max_length=50)
     population = models.IntegerField()
-    category = models.ForeignKey(SettlementType, blank=True, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
     location = models.ForeignKey(Location, blank=True, null=True, on_delete=models.SET_NULL)
     know_for = models.ForeignKey(KnownFor, blank=True, null=True, on_delete=models.SET_NULL)
     economy = models.ForeignKey(Economy, blank=True, null=True, on_delete=models.SET_NULL)
