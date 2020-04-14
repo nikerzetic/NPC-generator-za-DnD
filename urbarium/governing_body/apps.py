eb2df385a39c288a461368484339eefc691a76ef
Nik Erzetic <nikerzetic@gmail.com>
2020-04-14 14:24:10 +0200

    Dodal aplikacije za organ, tvorbo in naziv

---
+++ urbarium/governing_body/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GoverningBodyConfig(AppConfig):
+    name = 'governing_body'
---
+++ urbarium/political_formation/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PoliticalFormationConfig(AppConfig):
+    name = 'political_formation'
