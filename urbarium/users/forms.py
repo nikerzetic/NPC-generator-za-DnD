bfd05d0db9a329eb8dba6eb95b698ef337961f48
borutzupan <borutz.zupan@gmail.com>
2020-04-28 22:58:20 +0200

    Created an Update function for Profile and User

--- urbarium/users/forms.py
+++ urbarium/users/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
+# Create forms here
 
 # A form that inherits from UserCreationForm
 class UserRegisterForm(UserCreationForm):
@@ -15,3 +17,17 @@ class UserRegisterForm(UserCreationForm):
         model = User
         # fields that we want in the form
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = []
