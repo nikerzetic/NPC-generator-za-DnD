6da735bed8ad2f1cbf9723cdbc26e67ff30ead79
Nik Erzetic <nikerzetic@gmail.com>
2020-05-18 12:06:41 +0200

    Title model

--- urbarium/title/models.py
+++ urbarium/title/models.py
@@ -7,7 +7,7 @@ from character.models import Character
 class Title(models.Model):
     name = models.CharField(max_length=100)
     concerning = models.ForeignKey(GoverningBody, blank=True, null=True, on_delete=models.SET_NULL)
-    holders = models.ManyToManyField(Character, blank=True, null=True, on_delete=models.SET_NULL)
+    holders = models.ManyToManyField(Character)
 
     def __str__(self):
         return self.name
