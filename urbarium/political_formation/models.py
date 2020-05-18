b664481358b3bb3574f751614b0725983ad99fda
Nik Erzetic <nikerzetic@gmail.com>
2020-05-18 09:10:09 +0200

    Added title and political_formation model

--- urbarium/political_formation/models.py
+++ urbarium/political_formation/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from governing_body.models import GoverningBody
+
+
+class Classification(models.Model):
+    description = models.CharField(max_length=50)
+    def __str__(self):
+        return self.description
+
+class Government(models.Model):
+    description = models.CharField(max_length=50)
+    def __str__(self):
+        return self.description
+
+class PoliticalFormation(models.Model):
+    name = models.CharField(max_length=100)
+    classification = models.ForeignKey(Classification, blank=True, null=True, on_delete=models.SET_NULL)
+    government = models.ForeignKey(Government, blank=True, null=True, on_delete=models.SET_NULL)
+    governing_body = models.ForeignKey(GoverningBody, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
