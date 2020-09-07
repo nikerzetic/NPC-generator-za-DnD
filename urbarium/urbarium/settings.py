dce09c5148cf8766e9c36b84550abce37645e2a3
Nik Erzetic <nikerzetic@gmail.com>
2020-09-07 11:12:51 +0200

    Ponovna povezava na bazo

--- urbarium/urbarium/settings.py
+++ urbarium/urbarium/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'urbarium.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'sem2020_nike',
+        'USER': username,
+        'PASSWORD': password,
+        'HOST': 'baza.fmf.uni-lj.si',
+        'PORT': '5432',
     }
 }
 
