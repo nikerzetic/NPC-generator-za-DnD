bf8bcf786d322633a8f79025fc72ed39d52bb2af
Nik Erzetic <nikerzetic@gmail.com>
2020-09-09 18:09:43 +0200

    Homepage in povezava na admin v footer

--- urbarium/urbarium/views.py
+++ urbarium/urbarium/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'urbarium/homepage.html')
+
+def about(request):
+    return render(request, 'urbarium/about.html')
