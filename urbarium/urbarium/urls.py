d86229f4f7d5a2545d476f120e28ae8ad0085486
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2020-09-09 15:43:07 +0000

    Statične datoteke preko urls.py

--- urbarium/urbarium/urls.py
+++ urbarium/urbarium/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from urbarium.views import home
 
 
@@ -33,4 +35,4 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
     path('admin/', admin.site.urls),
-]
+] + static('static/', document_root=settings.STATICFILES_DIRS[0])
